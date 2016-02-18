858ea9258de22580d502cd9aeed51e38a331c93c
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-02-18 16:48:15 -0500

    Move static/ to root, import static properly

--- meetuppizza/settings.py
+++ meetuppizza/settings.py
@@ -125,13 +125,18 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
