bfe953fc9879c88482159db451c458526950c564
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-03-02 15:18:16 -0500

    add meetup model and tests

--- meetup/models.py
+++ meetup/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Meetup(models.Model):
-  name = models.CharField(max_length=500)
+  name = models.CharField(max_length=500, null=False, blank=False, default=None)
+  meetup_id = models.PositiveIntegerField()
--- meetup/tests.py
+++ meetup/tests.py
@@ -1,8 +1,22 @@
 from django.test import TestCase
 from meetup.models import Meetup
+from django.db import IntegrityError
 
 class TestMeetup(TestCase):
 
   def test_meetup_is_a_thing(self):
     meetup = Meetup()
     self.assertIsInstance(meetup, Meetup)
+
+  def test_create_meetup_with_name_and_id(self):
+    meetup = Meetup(name='papers we love', meetup_id=1)
+    self.assertEquals(meetup.name, 'papers we love')
+    self.assertEquals(meetup.meetup_id, 1)
+
+  def test_name_can_not_be_blank(self):
+    meetup = Meetup(meetup_id=1)
+    self.assertRaises(IntegrityError, meetup.save)
+
+  def test_meetup_id_can_not_be_blank(self):
+    meetup = Meetup(name='papers we love')
+    self.assertRaises(IntegrityError, meetup.save)
