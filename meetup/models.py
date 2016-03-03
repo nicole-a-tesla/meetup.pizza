65ab6b82d9255b8848a551d15c097716fdc5a9aa
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-03-03 15:51:58 -0500

    change meetup_id to meetup_link

--- meetup/models.py
+++ meetup/models.py
@@ -3,7 +3,7 @@ from pizzaplace.models import PizzaPlace
 
 class Meetup(models.Model):
   name = models.CharField(max_length=500, null=False, blank=False, default=None, unique=True)
-  meetup_id = models.PositiveIntegerField(unique=True)
+  meetup_link = models.CharField(max_length=500,unique=True, default=None)
   pizza_places = models.ManyToManyField(PizzaPlace)
 
   def __str__(self):
--- meetup/tests.py
+++ meetup/tests.py
@@ -10,44 +10,45 @@ class TestMeetup(TestCase):
     meetup = Meetup()
     self.assertIsInstance(meetup, Meetup)
 
-  def test_create_meetup_with_name_and_id(self):
-    meetup = Meetup(name='papers we love', meetup_id=1)
+  def test_create_meetup_with_name_and_link(self):
+    meetup = Meetup(name='papers we love', meetup_link='http://meetup.com/some-meetup')
     self.assertEquals(meetup.name, 'papers we love')
-    self.assertEquals(meetup.meetup_id, 1)
+    self.assertEquals(meetup.meetup_link, 'http://meetup.com/some-meetup')
 
   def test_name_can_not_be_blank(self):
-    meetup = Meetup(meetup_id=1)
+    meetup = Meetup(meetup_link='http://meetup.com/some-meetup')
     self.assertRaises(IntegrityError, meetup.save)
 
-  def test_meetup_id_can_not_be_blank(self):
+  def test_meetup_link_can_not_be_blank(self):
     meetup = Meetup(name='papers we love')
     self.assertRaises(IntegrityError, meetup.save)
 
   def test_name_can_not_be_over_500_characters(self):
     name = 'x' *501
-    meetup = Meetup(name=name, meetup_id=1)
+    meetup = Meetup(name=name, meetup_link='http://meetup.com/some-meetup')
     self.assertRaises(DataError, meetup.save)
 
   def test_string_representation(self):
-    m = Meetup(name="Mr. Meetup", meetup_id=4)
+    m = Meetup(name="Mr. Meetup", meetup_link='http://meetup.com/some-other-meetup')
     self.assertEquals("Mr. Meetup", str(m))
 
-  def test_meetuo_name_is_unique(self):
-    m = Meetup(name="Meetup", meetup_id=1)
+  def test_meetup_name_is_unique(self):
+    m = Meetup(name="Meetup", meetup_link='http://meetup.com/some-meetup')
     m.save()
-    n = Meetup(name="Meetup", meetup_id=2)
+    n = Meetup(name="Meetup", meetup_link='http://meetup.com/some-other-meetup')
     self.assertRaises(IntegrityError, n.save)
 
-  def test_meetup_name_is_unique(self):
-    m = Meetup(name="Meetup", meetup_id=1)  
+  def test_meetup_link_is_unique(self):
+    m = Meetup(name="Meetup", meetup_link='http://meetup.com/some-meetup')  
     m.save()
-    n = Meetup(name="Meetup new", meetup_id=1)
+    n = Meetup(name="Meetup new", meetup_link='http://meetup.com/some-meetup')
     self.assertRaises(IntegrityError, n.save)
 
   def test_getting_all_associated_pizzas(self):
-    meetup= Meetup(name="Meeetup1", meetup_id=1)
+    meetup= Meetup(name="Meeetup1", meetup_link='http://meetup.com/some-meetup')
     meetup.save()
     place = PizzaPlace(name="Pete Zazz")
     place.save()
     meetup.pizza_places.add(place)
     self.assertEquals(1, len(meetup.pizza_places.all()))
+
