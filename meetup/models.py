f81228e8a3ec865d35e336799d9890cee91f9809
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-03-03 10:01:30 -0500

    Add validation to make sure name and id are not blank on meetups

--- meetup/models.py
+++ meetup/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Meetup(models.Model):
-  name = models.CharField(max_length=500, null=False, blank=False, default=None)
-  meetup_id = models.PositiveIntegerField()
+  name = models.CharField(max_length=500, null=False, blank=False, default=None, unique=True)
+  meetup_id = models.PositiveIntegerField(unique=True)
 
   def __str__(self):
     return self.name
-
-
--- meetup/tests.py
+++ meetup/tests.py
@@ -30,3 +30,16 @@ class TestMeetup(TestCase):
   def test_string_representation(self):
     m = Meetup(name="Mr. Meetup", meetup_id=4)
     self.assertEquals("Mr. Meetup", str(m))
+
+  def test_meetuo_name_is_unique(self):
+    m = Meetup(name="Meetup", meetup_id=1)
+    m.save()
+    n = Meetup(name="Meetup", meetup_id=2)
+    self.assertRaises(IntegrityError, n.save)
+
+  def test_meetuo_name_is_unique(self):
+    m = Meetup(name="Meetup", meetup_id=1)
+    m.save()
+    n = Meetup(name="Meetup new", meetup_id=1)
+    self.assertRaises(IntegrityError, n.save)
+
