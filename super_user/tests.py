ecaf60536efcd840d2cdec8712ce249b37ed29e1
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-03-07 10:59:50 -0500

    Move super user tests to super user and delete unused files

--- super_user/tests.py
+++ super_user/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestSuperUser(TestCase):
+
+  def test_super_user_is_created_and_is_super(self):
+    user = User.objects.get(username='adminotaur')
+    self.assertTrue(user.is_staff)
+    self.assertTrue(user.is_superuser)
+
+  def test_super_user_is_created_with_hashed_password(self):
+    user = User.objects.get(username='adminotaur')
+    self.assertIn('pbkdf2_sha256$', user.password)
