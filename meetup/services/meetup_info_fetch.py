b82cf0417082be826bc818350c12b6a06d8865ae
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-03-09 11:11:04 -0500

    WIP mock setup

--- meetup/services/meetup_info_fetch.py
+++ meetup/services/meetup_info_fetch.py
@@ -4,7 +4,8 @@ import time
 import pdb
 
 class MeetupInfoFetch():
-  def __init__(self, meetups): 
+  def __init__(self, meetups, lookup_agent):
+    self.lookup_agent = lookup_agent
     self.meetups = meetups
 
   def fat_meetups(self):
@@ -19,15 +20,15 @@ class MeetupInfoFetch():
     return self.meetups
 
   def lookup_meetup_info(self, meetup):
-    lookup_agent = MeetupApiLookupAgent(meetup.meetup_link)
-    return lookup_agent.get_response('events').json()
+    lookup_agent_instance = self.lookup_agent(meetup.meetup_link)
+    return lookup_agent_instance.get_response('events').json()
 
   def append_info_to_meetup(self, meetup, event_info):
       meetup.venue = event_info[0]['venue']['name']
       meetup.next_event_topic = event_info[0]['name']
       meetup.datetime = self.parse_time(event_info[0]['time'])
       meetup.map_link = self.generate_map_link(event_info)
-      
+
 
   def parse_time(self, time_string):
     return time.ctime(int(time_string))[:-6]
@@ -35,4 +36,4 @@ class MeetupInfoFetch():
   def generate_map_link(self, event_info):
     lat = event_info[0]['group']['lat']
     lon = event_info[0]['group']['lon']
-    return "https://www.google.com/maps?q=%s,%s" % (lat, lon) 
+    return "https://www.google.com/maps?q=%s,%s" % (lat, lon)
