8cae132f9081ed30ce1ae5acd98f69d890d888e9
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-03-09 15:40:23 -0500

    Build new dictionary out of meetup object in info fetcher
    Info fetcher converts existing meetups into dictionaries, then
    appends event info to them if the api returns an upcoming event

--- meetup/services/meetup_info_fetch.py
+++ meetup/services/meetup_info_fetch.py
@@ -1,7 +1,6 @@
 from django.db import models
 from meetup.services.meetup_api_lookup_agent import MeetupApiLookupAgent
 import time
-import pdb
 
 class MeetupInfoFetch():
   def __init__(self, meetups, lookup_agent):
@@ -14,20 +13,33 @@ class MeetupInfoFetch():
     for meetup in self.meetups:
       event_info = self.lookup_meetup_info(meetup)
 
-      if event_info:
-        self.append_info_to_meetup(meetup, event_info)
+      new_meetup = self.build_meetup_info_dictionary(meetup, event_info)
+      new_meetups.append(new_meetup)
 
-    return self.meetups
+    return new_meetups
 
   def lookup_meetup_info(self, meetup):
     lookup_agent_instance = self.lookup_agent(meetup.meetup_link)
     return lookup_agent_instance.get_response('events').json()
 
-  def append_info_to_meetup(self, meetup, event_info):
-      meetup.venue = event_info[0]['venue']['name']
-      meetup.next_event_topic = event_info[0]['name']
-      meetup.datetime = self.parse_time(event_info[0]['time'])
-      meetup.map_link = self.generate_map_link(event_info)
+  def convert_meetup_object_to_dictionary(self, meetup):
+    new_meetup = {}
+    new_meetup['name'] = meetup.name
+    new_meetup['meetup_link'] = meetup.meetup_link
+    new_meetup['pizza_places'] = meetup.pizza_places.all()
+    return new_meetup
+
+
+  def build_meetup_info_dictionary(self, meetup, event_info):
+      new_meetup = self.convert_meetup_object_to_dictionary(meetup)
+
+      if event_info:
+        new_meetup['venue'] = event_info[0]['venue']['name']
+        new_meetup['next_event_topic'] = event_info[0]['name']
+        new_meetup['datetime'] = self.parse_time(event_info[0]['time'])
+        new_meetup['map_link'] = self.generate_map_link(event_info)
+
+      return new_meetup
 
 
   def parse_time(self, time_string):
