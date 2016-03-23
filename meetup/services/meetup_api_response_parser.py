3feb6d0f119f28404e7aa808dfd27271402ce330
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-03-23 13:49:06 -0400

    Refactor meetup_parser and add test for edge case where venue is not returned

--- meetup/services/meetup_api_response_parser.py
+++ meetup/services/meetup_api_response_parser.py
@@ -1,29 +1,21 @@
+
+def get_nested_venue_attr_or_default(json, nested_attr):
+  venue = json[0].get('venue')
+
+  if venue:
+    return venue.get(nested_attr)
+  else:
+    return "No %s listed" %nested_attr
+
+
 def parse(api_response):
   json = api_response.json()
   parsed_response = {}
 
   if json:
-
-    try:
-      parsed_response['venue'] = json[0].get('venue').get('name')
-    except AttributeError:
-      parsed_response['venue'] = "No venue listed"
-
-    try:
-      parsed_response['next_event_topic'] = json[0].get('name')
-    except AttributeError:
-      parsed_response['next_event_topic'] = "No name listed"
-
-    try:
-      parsed_response['datetime'] = json[0].get('time')
-    except AttributeError:
-      parsed_response['datetime'] = "No time listed"
-
-    try:
-      parsed_response['lat'] = json[0].get('venue').get('lat')
-      parsed_response['lon'] = json[0].get('venue').get('lon')
-    except AttributeError:
-      parsed_response['lat'] = '40.689745'
-      parsed_response['lon'] = '-74.0476567'
-
+    parsed_response['venue'] = get_nested_venue_attr_or_default(json, 'name')
+    parsed_response['lat'] = get_nested_venue_attr_or_default(json, 'lat')
+    parsed_response['lon'] = get_nested_venue_attr_or_default(json, 'lon')
+    parsed_response['next_event_topic'] = json[0].get('name', 'No topic listed')
+    parsed_response['datetime'] = json[0].get('time', 'No time listed')
   return parsed_response
--- meetup/tests.py
+++ meetup/tests.py
@@ -202,18 +202,47 @@ class TestMeetupPresenter(TestCase):
 
 class TestMeetupApiResponseParser(TestCase):
   def setUp(self):
-    self.mockResponse = HttpResponse()
-    self.mockResponse.json = MagicMock(return_value=meetup_api_response)
+    self.mock_response = HttpResponse()
+    self.mock_response.json = MagicMock(return_value=meetup_api_response)
 
   def test_parsed_response_contains_venue(self):
-    self.assertEquals(meetup_api_response_parser.parse(self.mockResponse).get('venue'), 'The Lexington')
+    self.assertEquals(meetup_api_response_parser.parse(self.mock_response).get('venue'), 'The Lexington')
 
   def test_parsed_response_contains_event_topic(self):
-    self.assertEquals(meetup_api_response_parser.parse(self.mockResponse).get('next_event_topic'), 'Code & Coffee')
+    self.assertEquals(meetup_api_response_parser.parse(self.mock_response).get('next_event_topic'), 'Code & Coffee')
 
   def test_parsed_response_contains_event_datetime(self):
-    self.assertEquals(meetup_api_response_parser.parse(self.mockResponse).get('datetime'), 1458730800000)
+    self.assertEquals(meetup_api_response_parser.parse(self.mock_response).get('datetime'), 1458730800000)
 
   def test_parsed_response_contains_lat_and_long(self):
-    self.assertEquals(meetup_api_response_parser.parse(self.mockResponse).get('lat'), 40.75501251220703)
-    self.assertEquals(meetup_api_response_parser.parse(self.mockResponse).get('lon'), -73.97337341308594)
+    self.assertEquals(meetup_api_response_parser.parse(self.mock_response).get('lat'), 40.75501251220703)
+    self.assertEquals(meetup_api_response_parser.parse(self.mock_response).get('lon'), -73.97337341308594)
+
+  def test_handles_response_with_no_venue(self):
+    venueless_response = [{
+          "created": 1426723243000,
+          "duration": 5400000,
+          "group": {
+            "created": 1391476627000,
+            "name": "Software Craftsmanship New York",
+            "id": 12705402,
+            "join_mode": "approval",
+            "lat": 40.7599983215332,
+            "lon": -73.98999786376953,
+            "urlname": "Software-Craftsmanship-New-York",
+            "who": "craftsmen"
+          },
+          "id": "ldxfglyvfbfc",
+          "link": "http://www.meetup.com/Software-Craftsmanship-New-York/events/229387692/",
+          "name": "Code & Coffee",
+          "status": "upcoming",
+          "time": 1458730800000,
+          "updated": 1453163859000,
+          "utc_offset": -14400000,
+          "yes_rsvp_count": 1,
+          "waitlist_count": 0,
+          "description": "<p>Do you like getting up early and starting the day with inspiring conversations or even better",
+          }
+        ]
+    self.mock_response.json = MagicMock(return_value=venueless_response)
+    self.assertEquals(meetup_api_response_parser.parse(self.mock_response).get('venue'), 'No name listed')
