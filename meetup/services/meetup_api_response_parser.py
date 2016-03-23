bb9fa76ee20bfb9dc69cef9275303338c9988d38
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-03-23 18:50:19 -0400

    WIP Refactor meetup api response parser some more

--- meetup/services/meetup_api_response_parser.py
+++ meetup/services/meetup_api_response_parser.py
@@ -6,31 +6,27 @@ class MeetupApiResponseParser():
   def json(self):
     return self.http_response.json()[0]
 
-  # def get_nested_attribute(self, nested_attr):
-  #   venue = self.json[0].get('venue')
+  def extract_attribute(self, context, *args):
+    current_key = args[0]
+    not_found_message = "No %s listed" %current_key
+    value_at_current_key = context.get(current_key, not_found_message)
 
-  #   if venue:
-  #     return venue.get(nested_attr)
-  #   else:
-  #     return "No %s listed" %nested_attr
+    if self.exit_condition_is_true(context, args):
+      return value_at_current_key
 
-  def extract_attribute(self, dic, *args):
-    find_this = args[0]
-    not_found_message = "No %s listed" %find_this
+    return self.extract_attribute(value_at_current_key, args[1])
 
-    if len(args) == 1 or dic.get(find_this) == None:   # Or there's nothing at venue
-      return dic.get(find_this, not_found_message)
-
-    new_dic = dic.get(find_this, not_found_message)
-    return self.extract_attribute(new_dic, args[1])
-
+  def exit_condition_is_true(self, dic, args):
+    this_is_the_last_arg = len(args) == 1
+    no_matching_element_found = dic.get(args[0]) == None
+    return this_is_the_last_arg or no_matching_element_found
 
   def parse(self):
     if self.json:
       return {
-        'venue': self.extract_attribute(self.json, 'venue', 'name'),
-        'lat' : self.extract_attribute(self.json, 'venue', 'lat'),
-        'lon' : self.extract_attribute(self.json, 'venue', 'lon'),
+        'venue'           : self.extract_attribute(self.json, 'venue', 'name'),
+        'lat'             : self.extract_attribute(self.json, 'venue', 'lat'),
+        'lon'             : self.extract_attribute(self.json, 'venue', 'lon'),
         'next_event_topic': self.extract_attribute(self.json, 'name'),
-        'datetime': self.extract_attribute(self.json, 'time')
+        'datetime'        : self.extract_attribute(self.json, 'time')
       }
