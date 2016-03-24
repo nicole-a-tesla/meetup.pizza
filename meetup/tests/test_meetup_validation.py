90eb564306083a0b8cdecfb191f6b90e4d8ae74d
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-03-24 15:10:50 -0400

    Add specific error messages to validation tests in pizza places and meetups

--- meetup/tests/test_meetup_validation.py
+++ meetup/tests/test_meetup_validation.py
@@ -10,6 +10,7 @@ class TestMeetupModelValidations(TestCase):
   def setUp(self):
     self.patcher = patch('meetup.models.MeetupClient')
     self.mock_agent = self.patcher.start()
+    self.invalid_url_format_message = "Url should be in form 'http://meetup.com/meetup-name'"
 
   def test_meetup_raises_error_on_invalid_url(self):
     meetup= Meetup(name="Meetup1", meetup_url='hi/ok/what')
@@ -17,19 +18,23 @@ class TestMeetupModelValidations(TestCase):
 
   def test_error_raised_if_url_does_not_point_to_meetupdotcom(self):
     meetup= Meetup(name="Meetup1", meetup_url='http://www.example.com/')
-    self.assertRaises(ValidationError, meetup.full_clean)
+    with self.assertRaisesRegexp(ValidationError, self.invalid_url_format_message):
+      meetup.full_clean()
 
   def test_error_raised_if_no_urlname_in_meetup_url(self):
     meetup= Meetup(name="Meetup1", meetup_url='http://www.meetup.com/')
-    self.assertRaises(ValidationError, meetup.full_clean)
+    with self.assertRaisesRegexp(ValidationError, self.invalid_url_format_message):
+      meetup.full_clean()
 
   def test_error_raised_if_no_trailing_slash_in_meetup_url(self):
     meetup= Meetup(name="Meetup1", meetup_url='http://www.meetup.com/hackerhours')
-    self.assertRaises(ValidationError, meetup.full_clean)
+    with self.assertRaisesRegexp(ValidationError, self.invalid_url_format_message):
+      meetup.full_clean()
 
   def test_error_raised_if_multiple_urlnames(self):
     meetup= Meetup(name="Meetup1", meetup_url='http://www.meetup.com/hackerhours/events/')
-    self.assertRaises(ValidationError, meetup.full_clean)
+    with self.assertRaisesRegexp(ValidationError, self.invalid_url_format_message):
+      meetup.full_clean()
 
   def test_meetup_url_with_urlname_and_trailing_slash_passes(self):
     meetup= Meetup(name="Meetup1", meetup_url='http://www.meetup.com/hackerhours/')
@@ -44,7 +49,8 @@ class TestMeetupModelValidations(TestCase):
   def test_non_real_meetup_raises_validation_error(self):
     self.mock_agent.return_value.exists.return_value = False
     meetup = Meetup(name="Meetup1", meetup_url='http://www.meetup.com/la-la-la/')
-    self.assertRaises(ValidationError, meetup.full_clean)
+    with self.assertRaisesRegexp(ValidationError, 'Meetup not found on meetup.com'):
+      meetup.full_clean()
 
   def tearDown(self):
     self.addCleanup(self.patcher.stop)
